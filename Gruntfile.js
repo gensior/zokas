6b01aac0c5413090b0600e49dcd61c3c513c283b
gensior <j.franceschini@gmail.com>
2013-11-02 21:06:28 -0700

    Adds jshint task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,54 @@
-module.exports = function(grunt) {
+(function() {
+  'use strict';
 
-  grunt.registerTask("default", "Default description", function () {
-    console.log("Hello world");
-  });
+  module.exports = function(grunt) {
 
-};
+    grunt.initConfig({
+      jshint: {
+        main: {
+          options: nodeLintOptions(),
+          files: {
+            src: ["app.js", "Gruntfile.js"]
+          }
+        }
+      }
+    });
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+
+    grunt.registerTask("default", "Default description", ["jshint"], function () {
+      console.log("Hello world");
+    });
+
+    function globalLintOptions() {
+      return {
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: false,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        regexp: true,
+        undef: true,
+        strict: true,
+        trailing: true
+      };
+    }
+
+    function nodeLintOptions() {
+      var options = globalLintOptions();
+      options.node = true;
+      return options;
+    }
+
+    function browserLintOptions() {
+      var options = globalLintOptions();
+      options.browser = true;
+      return options;
+    }
+
+  };
+})();
